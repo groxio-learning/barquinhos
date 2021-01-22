8891727126449580affe14a0321228bf99eea5d0
Cristine Guadelupe <cristineguadelupe@me.com>
2021-01-22 12:35:57 -0300

    Place vertical destroyers on the board

--- lib/barquinhos_web/live/game_live.ex
+++ lib/barquinhos_web/live/game_live.ex
@@ -1,6 +1,6 @@
 defmodule BarquinhosWeb.GameLive do
   use BarquinhosWeb, :live_view
-  alias Barquinhos.Game.Board
+  alias Barquinhos.Game.{Board, Ship}
 
   def mount(_params, _session, socket) do
     {:ok, socket |> build}
@@ -10,10 +10,15 @@ defmodule BarquinhosWeb.GameLive do
     socket
     |> ships()
     |> board()
+    |> points()
   end
 
   defp ships(socket) do
     assign(socket, ships: [])
+  end
+
+  defp points(socket) do
+    assign(socket, points: [])
   end
 
   defp board(socket) do
@@ -21,11 +26,25 @@ defmodule BarquinhosWeb.GameLive do
   end
 
   defp ships(%{assigns: %{ships: ships}} = socket, ship) do
-    assign(socket, ships: [ship | ships])
+    assign(socket, ships: [new_ship(ship) | ships])
   end
 
-  def handle_event("slot", %{"x" => x, "y" => y}, socket) do
-    IO.inspect({x, y})
-    {:noreply, socket |> ships(x <> y)}
+  defp new_ship(points) do
+    Ship.new(points, :vertical, :destroyer)
   end
+
+  defp to_points(%{assigns: %{ships: ships}} = socket) do
+    my_ships =
+      ships
+      |> Enum.map(& Ship.to_points/1)
+      |> List.flatten()
+      |> Enum.map(fn {x, y} -> "#{x}#{y}" end)
+      
+    assign(socket, points: my_ships)
+  end
+
+  def handle_event("add_ship", %{"x" => x, "y" => y}, socket) do
+    {:noreply, socket |> ships({String.to_integer(x), String.to_integer(y)}) |> to_points()}
+  end
+
 end
