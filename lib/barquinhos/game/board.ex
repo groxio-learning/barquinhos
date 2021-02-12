51cea56ad687d5efbddb211f5bf5968931cc988a
Herminio Torres <herminiocesar@gmail.com>
2021-02-11 21:38:17 -0300

    change some public function in a private functions

--- lib/barquinhos/game/board.ex
+++ lib/barquinhos/game/board.ex
@@ -26,30 +26,28 @@ defmodule Barquinhos.Game.Board do
     Enum.any?(board.ships, fn ship -> Ship.hit?(ship, shot) end)
   end
 
-  def maybe_ship_type(nil, _sunk), do: nil
-  def maybe_ship_type(_ship, false), do: nil
-  def maybe_ship_type(ship, true), do: ship.type
+  defp maybe_ship_type(nil, _sunk), do: nil # TODO: I think this function its not use
+  defp maybe_ship_type(_ship, false), do: nil
+  defp maybe_ship_type(ship, true), do: ship.type
 
-  def sunk_ship_name(%__MODULE__{shots: [last_shot|_rest]} = board) do
+  defp sunk_ship(%__MODULE__{shots: [last_shot|_rest]} = board) do
     hit_ship = board.ships
     |> Enum.find(fn ship -> Ship.hit?(ship, last_shot) end) #ship or nil
 
     maybe_ship_type(hit_ship, Ship.sunk?(hit_ship, last_shot))
   end
-  def sunk_ship_name(_), do: nil
+  defp sunk_ship(_), do: nil
 
-  def game_over?(%__MODULE__{} = board) do
+  defp game_over?(%__MODULE__{} = board) do
     Enum.all?(board.ships, fn ship -> Ship.sunk?(ship, board.shots) end)
   end
 
   def status(board, shot) do
-    sunk_ship = sunk_ship_name(board)
     cond do
       game_over?(board) -> %{board | status: :gameover}
       not hit?(board, shot) -> %{board | status: :miss}
-      is_nil(sunk_ship) -> %{board | status: :hit}
-      true -> %{board | status: sunk_ship}
+      is_nil(sunk_ship(board)) -> %{board | status: :hit}
+      true -> %{board | status: sunk_ship(board)}
     end
   end
-
 end
