a081b33a194cbe921c03a197b91d29d4508e2b22
Cristine Guadelupe <Rekviem@me.com>
2021-03-02 22:18:05 -0300

    Create the player module

---
+++ lib/barquinhos/game/player.ex
@@ -0,0 +1,17 @@
+defmodule Barquinhos.Game.Player do
+  alias Barquinhos.Game.Board
+  defstruct [:id, :nickname, winner: false, board: %Board{}, shots: []]
+
+  def new(nickname) do
+    %__MODULE__{
+      id: generate_id(),
+      nickname: nickname
+    }
+  end
+
+  def add_shot(player, shot) do
+    %{player | shots: [shot | player.shots]}
+  end
+
+  defp generate_id(), do: Enum.random(1000..9999)
+end
