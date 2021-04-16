0cc9f83929ae3df4e967baf0b1e312c31ff511ee
mohammed hindawash <djslakivich@gmail.com>
2021-04-15 20:10:51 -0400

    Added a check to see if both players are ready before you're able to attack

--- lib/barquinhos_web/live/game_live.ex
+++ lib/barquinhos_web/live/game_live.ex
@@ -26,6 +26,11 @@ defmodule BarquinhosWeb.GameLive do
     |> ship_type(nil)
     |> ship_orientation(nil)
     |> game_status(:placing)
+    |> every_player_ready()
+  end
+
+  def every_player_ready(socket) do
+    assign(socket, every_player_ready: false)
   end
 
   defp players(socket) do
@@ -102,6 +107,7 @@ defmodule BarquinhosWeb.GameLive do
 
   def all_players_ready?([]), do: false
 
+  #for all of the players, take each player and check to see if it's ready
   def all_players_ready?(players) do
     Enum.all?(players, fn player -> player.ready end)
   end
@@ -125,14 +131,18 @@ defmodule BarquinhosWeb.GameLive do
   end
 
   def handle_event("add_shot", %{"x" => x, "y" => y}, socket) do
-    BarquinhosWeb.Endpoint.broadcast("battleship", "shot_fired", %{
-      "player" => socket.assigns.player,
-      "x" => x,
-      "y" => y
-    })
+    if all_players_ready?(socket.assigns.players) do
+      BarquinhosWeb.Endpoint.broadcast("battleship", "shot_fired", %{
+        "player" => socket.assigns.player,
+        "x" => x,
+        "y" => y
+      })
 
-    {:noreply,
-     assign(socket, shots: [{String.to_integer(x), String.to_integer(y)} | socket.assigns.shots])}
+      {:noreply,
+       assign(socket, shots: [{String.to_integer(x), String.to_integer(y)} | socket.assigns.shots])}
+     else
+       {:noreply, socket}
+     end
   end
 
   def handle_event("ship_type", %{"type" => ship}, socket) do
