bef36e980d25ddb19466d9923c3661d3d8e46a44
Grace McPherson <grace.theprogrammer@gmail.com>
2021-05-06 20:13:17 -0400

    refactoring liveview by moving logic into board.ex

--- lib/barquinhos/game/board.ex
+++ lib/barquinhos/game/board.ex
@@ -2,22 +2,40 @@ defmodule Barquinhos.Game.Board do
   alias Barquinhos.Game.Ship
 
   # ship-hit, miss, ship-sunk, gameover, ready
-  defstruct ships: [], shots: [], status: :ready
+  defstruct ships: [], shots: [], opponent_hits: [], status: :ready
+  #shots_sent,  successful_shots
+  #direct hit
 
   # constructor
   def new do
     %__MODULE__{}
   end
 
+  # #reference
+  #   defp ships(%{assigns: %{ships: ships}} = socket, ship) do
+  #     assign(socket, ships: [new_ship(socket, ship) | ships])
+  #   end
+  #
+  #   defp new_ship(%{assigns: %{ship_type: ship_type, ship_orientation: ship_orientation}}, points) do
+  #     Ship.new(points, ship_orientation, ship_type)
+  #   end
+
   # reducer
-  def add_ship(%__MODULE__{} = board, ship) do
-    # Add ship to board's ship
+  def add_ship(%__MODULE__{} = board, x, y, ship_orientation, ship_type) do
+    starting_point = {String.to_integer(x), String.to_integer(y)}
+    ship = Ship.new(starting_point, ship_orientation, ship_type)
     %{board | ships: [ship | board.ships]}
   end
 
   # reducer
-  def attack(%__MODULE__{} = board, shot) do
+  def attack(%__MODULE__{} = board, x, y) do
+    shot = {String.to_integer(x), String.to_integer(y)}
     %{board | shots: [shot | board.shots]}
+  end
+
+  def add_opponent_hit(%__MODULE__{} = board, x, y) do
+    shot = {String.to_integer(x), String.to_integer(y)}
+    %{board | opponent_hits: [shot | board.opponent_hits]}
   end
 
   # converter
